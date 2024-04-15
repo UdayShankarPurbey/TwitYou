9b8e66cffc7a3ece93d2e333e2f48b20f6bab94d
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-15 15:50:21 +0530

    Basic Project Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : '' , component : PageLayoutComponent}
+];
---
+++ src/app/components/page-layout/page-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-layout.component.html',
+  styleUrl: './page-layout.component.css'
+})
+export class PageLayoutComponent {
+
+}
