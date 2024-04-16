c63bae713ab162365332ced11e7f297a32280e73
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-16 09:41:40 +0530

    Add Antd Design

--- src/app/components/page-layout/page-layout.component.ts
+++ src/app/components/page-layout/page-layout.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 @Component({
   selector: 'app-page-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule
+  ],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.css'
 })
 export class PageLayoutComponent {
+  isCollapsed = false;
 
 }
